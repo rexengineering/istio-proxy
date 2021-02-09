64799f5ba5a496c0d75302aa36e7b170789b5a23
Meta Engineer <m@plankc.com>
2021-02-09 00:01:27 -0600

    Adds all of the BAVS and DataTraceLogger code.

---
+++ rebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Starting proxybuilder container. If this fails, try running buildtools/setup_build.sh"
+docker start proxybuilder
+
+echo "Successfully started the proxybuilder container. Commencing build."
+docker exec -it proxybuilder /build.sh
+
+echo "Done building. Copying Envoy Binary."
+docker cp proxybuilder:/proxy/bazel-bin/src/envoy/envoy buildtools/
+
+echo "Done copying Envoy Binary. Building final REX Istio-Proxy image."
+docker build -t 'rex-proxy:1.8.2' -f Dockerfile.localproxy buildtools/
