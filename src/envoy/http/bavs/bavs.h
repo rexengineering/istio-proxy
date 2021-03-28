4d6751054ff679e8d3c5c39d5e76629f22966dab
Meta Engineer <m@plankc.com>
2021-03-28 09:29:10 -0500

    moves BavsOutboundCallbacks implementation from .h file to outbound_callbacks.cc

--- src/envoy/http/bavs/bavs.h
+++ src/envoy/http/bavs/bavs.h
@@ -104,92 +104,20 @@ public:
         cluster_manager_->storeCallbacksAndHeaders(id, this);
     }
 
-    void onHeaders(Http::ResponseHeaderMapPtr&& headers, bool) override {
-        // TODO: Allow the user to configure a list of "retriable" headers.
-        std::string status_str(headers->getStatusValue());
-        int status = atoi(status_str.c_str());
-        retriable_failure_ = false;
-        unretriable_failure_ = false;
-
-        // Retry on all 5xx's
-        if (status >= 500) {
-            retriable_failure_ = true;
-        } else if (!retriable_failure_ && (status < 200 || status >= 300)) {
-            unretriable_failure_ = true;
-        }
-
-        // If we have an unretriable failure OR we run out of retry attempts,
-        // we must redirect to notify Flowd of our failure.
-        if (unretriable_failure_ || attempts_left_ == 1) {
-            // We're sending back to Flowd rather than to the initial destination, so we
-            // need to tell Flowd what the original host+path headers were.
-            headers_->setCopy(Http::LowerCaseString("x-rexflow-original-path"), headers_->getPathValue());
-            headers_->setCopy(Http::LowerCaseString("x-rexflow-original-host"), headers_->getHostValue());
-            headers_->setPath(fail_cluster_path_);
-            cluster_ = fail_cluster_;
-        }
-    }
-
+    void onHeaders(Http::ResponseHeaderMapPtr&& headers, bool) override;
+    void onComplete() override;
     void onData(Buffer::Instance&, bool) override {}
+
     void onTrailers(Http::ResponseTrailerMapPtr&&) override {}
     void onReset() override {}
-    void onComplete() override {
-        if ((unretriable_failure_ || retriable_failure_) && attempts_left_) {
-            doRetry(headers_only_);
-        }
-        // Finally, remove ourself from the clusterManager
-        cluster_manager_->eraseCallbacksAndHeaders(id_);
-    }
-    Http::RequestHeaderMapImpl& requestHeaderMap() override {
-        return *(headers_.get());
-    }
-
-    void setStream(Http::AsyncClient::Stream* stream) override { request_stream_ = stream;}
-    Http::AsyncClient::Stream* getStream() override { return request_stream_; }
-
-    void addData(Buffer::Instance& data) {
-        buffer_->add(data);
-    }
+    Http::RequestHeaderMapImpl& requestHeaderMap() override;
+    void setStream(Http::AsyncClient::Stream* stream) override;
+    Http::AsyncClient::Stream* getStream() override;
+    void addData(Buffer::Instance& data);
 
 private:
 
-    void doRetry(bool end_stream) {
-        Http::AsyncClient* client = nullptr;
-        try {
-            client = &(cluster_manager_->httpAsyncClientForCluster(cluster_));
-        } catch(const EnvoyException&) {
-            std::cout << "Couldn't find the cluster " << cluster_ << std::endl;
-        }
-        if (client == nullptr) return;
-
-        Random::RandomGeneratorImpl rng;
-        std::string new_id = rng.uuid();
-        BavsOutboundCallbacks *cb = new BavsOutboundCallbacks(new_id, std::move(headers_),
-                *cluster_manager_, attempts_left_ - 1, fail_cluster_, cluster_,
-                fail_cluster_path_, config_);
-        cb->addData(*buffer_);
-
-        auto stream = client->start(*cb, AsyncClient::StreamOptions());
-        cb->setStream(stream);
-        if (cb->getStream()) {
-            cb->getStream()->sendHeaders(cb->requestHeaderMap(), end_stream);
-        }
-        if (!end_stream) {
-            // After sending, the connection may have been auto-closed if the service is down.
-            // Therefore, we have to re-check to make sure that the `cb` is still valid and its
-            // stream is also still valid.
-            cb = static_cast<BavsOutboundCallbacks*>(cluster_manager_->getCallbacksAndHeaders(new_id));
-            if (cb && cb->getStream()) {
-                cb->getStream()->sendData(*buffer_, true);
-
-                // Once again, there's a chance that it was terminated. Check for closure again.
-                cb = static_cast<BavsOutboundCallbacks*>(cluster_manager_->getCallbacksAndHeaders(new_id));
-                Buffer::OwnedImpl empty_buffer;
-                if (cb && cb->getStream()) cb->getStream()->sendData(empty_buffer, true);
-            }
-        }
-    }
-
+    void doRetry(bool end_stream);
     std::string id_;
     Upstream::ClusterManager* cluster_manager_;
     int attempts_left_;
@@ -199,7 +127,6 @@ private:
     std::unique_ptr<Buffer::OwnedImpl> buffer_;
     bool headers_only_ = false;
     std::string fail_cluster_path_;
-
     bool unretriable_failure_ = false;
     bool retriable_failure_ = false;
     Http::AsyncClient::Stream* request_stream_;
@@ -249,6 +176,10 @@ private:
     std::string context_input_;
     bool inbound_data_is_json_;
     bool did_update_request_data_;
+
+};
+
+class BavsErrorCallbacks : public Envoy::Upstream::AsyncStreamCallbacksAndHeaders {
 
 };
 
